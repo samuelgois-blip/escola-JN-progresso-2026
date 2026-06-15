b3570748f34c90eae823cbd84ffe850d3d35d813
samuel.gois <samuel.gois@aluno.edu.pi.gov.br>
2026-06-15 15:14:40 +0000

    adiciona modulo de calculadora e uso de variaveis

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const calculadora = require('./calculadora')
 
 
 let soma = calculadora.somar (18, 5);
-let subtração = calculadora. subtrair(10, 5);
+let subtração = calculadora.subtrair(10, 5);
 let multiplicacao = calculadora.multiplicar(10, 5);
 let divisao = calculadora.dividir(10, 5);
 console.log("soma:", soma);
-console.log("subtração", sutracao);
+console.log("subtração", subtração);
 console.log("multiplicacao",multiplicacao);
 console.log("divisao", divisao);
